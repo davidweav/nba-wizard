9931ada6733a056ab25c64b079324866b0b47384
David Weaver <davidweav322@gmail.com>
2024-02-10 16:57:10 -0500

    updates homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,25 @@
 import Image from 'next/image'
 import Link from 'next/link'
 
+
+
 export default function Home() {
+
+  function fetchData() {
+    console.log("hello");
+  }
+
+  
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="flex flex-row justify-end">
-        <h1 >NBA Wizard</h1>
-        <h1>Test</h1>
+    <main className="homepage">
+      <div className="nav">
+        <h1 className="title" >NBA Wizard</h1>
+        <Image src="/logo.png" alt="basketball" width={100} height={100}/>
       </div>
+      <p>We pick it you hit it!</p>
+   
+      <button onClick={fetchData}>Fetch Recent Picks</button>
+      
       
     </main>
   )
