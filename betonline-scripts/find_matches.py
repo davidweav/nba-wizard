9fece410e6b051162cef592c5efab799c1db57db
adamruehle <adamruehle@live.com>
2024-02-10 19:03:42 -0500

    created script to find matches between betonline and underdogs

---
+++ betonline-scripts/find_matches.py
@@ -0,0 +1,49 @@
+import csv
+import pandas as pd
+
+try:
+    df1 = pd.read_csv('betonline-scripts/betonline-odds/nba_player_odds_2024-02-10.csv')
+    df2 = pd.read_csv('betonline-scripts/underdog-lines/player_data_2024-02-10.csv')
+except KeyError as e:
+    print(f"Missing column: {e}")
+    exit()
+
+matching_lines = []
+properties = ['Assists', 'PointsReboundsAssists', 'Points', 'Rebounds', 'Threes']
+
+for index, row1 in df1.iterrows():
+    player_name = row1['Player']
+    
+    # Find the corresponding row in the second DataFrame
+    matching_row = df2[df2['Name'] == player_name]
+    
+    # Check if the matching row exists
+    if not matching_row.empty:
+        # Iterate over the properties
+        for prop in properties:
+            if prop + '_Line' in row1 and prop + '_Line' in matching_row:
+                prop_line_df1 = row1[prop + '_Line']
+                prop_line_df2 = matching_row[prop + '_Line'].iloc[0]
+                
+                # Check if any required value is missing
+                if pd.notna(prop_line_df1) and pd.notna(prop_line_df2):
+                    # Compare the property lines between df1 and df2
+                    if prop_line_df1 == prop_line_df2:
+                        if row1[prop + '_Over'] < row1[prop + '_Under']:
+                            odds_value = row1[prop + '_Over']
+                            odds_string = 'Over'
+                        else:
+                            odds_value = row1[prop + '_Under']
+                            odds_string = 'Under'
+                        # Save the matching lines along with corresponding 'Over' and 'Under' values from df1
+                        matching_lines.append({
+                            'name': player_name,
+                            'proptype': prop,
+                            'line': prop_line_df1,
+                            'odds': odds_value,
+                            'type': odds_string
+                        })
+                else:
+                    print(f"Warning: Missing data for {player_name} in one or both dataframes.")
+
+print(matching_lines)
---
+++ betonline-scripts/scrape_underdogs.py
@@ -0,0 +1,103 @@
+import os
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+import time
+import datetime
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import csv
+from datetime import datetime
+
+options = webdriver.ChromeOptions()
+# options.add_argument("--headless")
+options.add_argument("--log-level=3")
+options.add_argument("--disable-logging")
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+driver.get("https://underdogfantasy.com/pick-em/higher-lower/all/nba")
+driver.maximize_window()
+time.sleep(3)
+email_input = driver.find_element(By.CSS_SELECTOR, '.styles__field__Q6LKF [data-testid="email_input"]')
+email_input.send_keys("adamruehle@live.com")
+password_input = driver.find_element(By.CSS_SELECTOR, '.styles__field__Q6LKF [data-testid="password_input"]')
+password_input.send_keys("mT4zt2sJaYT88BPb@KG!WG_YK")
+sign_in_button = driver.find_element(By.CSS_SELECTOR, '[data-testid="sign-in-button"]')
+sign_in_button.click()
+time.sleep(2)
+# driver.get("https://underdogfantasy.com/pick-em/higher-lower/all/nba")
+driver.get("https://underdogfantasy.com/pick-em/higher-lower/pre-game/nba")
+
+def find_player_and_props(driver, player_prop_div):
+    more_picks_button = WebDriverWait(player_prop_div, 10).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "button")))[-1]
+    try:
+        more_picks_span = more_picks_button.find_element(By.CSS_SELECTOR, 'span')
+        if "More picks" in more_picks_span.text:
+            driver.execute_script("arguments[0].scrollIntoView({behavior: 'smooth', block: 'center'});", more_picks_button)
+            more_picks_button.click()
+    except:
+        None
+    player_name_element = WebDriverWait(player_prop_div, 10).until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'h1.styles__playerName__jW6mb')))
+    player_name = player_name_element.text
+    props = []
+    stat_line_elements = player_prop_div.find_elements(By.CSS_SELECTOR, 'div.styles__overUnderListCell__tbRod')
+    player_data = {'Name': player_name}
+    for stat_line_element in stat_line_elements:
+        prop = stat_line_element.find_element(By.CSS_SELECTOR, 'p').text
+        if "1H" in prop or "1Q" in prop:
+            continue
+        prop_name, value = prop.split(' ', 1)  # Split by the first space
+        player_data[prop_name] = value
+        props.append(prop)
+
+    return player_name, props
+
+def write_to_csv(data):
+    current_date = datetime.now().strftime("%Y-%m-%d")
+    csv_file_path = f'underdog-lines/player_data_{current_date}.csv'
+    directory = os.path.dirname(csv_file_path)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    # Extract all unique property names from data
+    all_props = [prop for player_data in data for prop in player_data.keys() if prop != 'Name']
+    unique_props = list(set(all_props))
+    # Define fieldnames including 'Name' and unique properties
+    fieldnames = ['Name'] + unique_props
+    with open(csv_file_path, 'a', newline='') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        if csvfile.tell() == 0:
+            writer.writeheader()
+        # Write data to CSV file
+        for player_data in data:
+            # Create dictionary with all properties and their corresponding values
+            player_dict = {'Name': player_data['Name']}
+            for prop in unique_props:
+                player_dict[prop] = player_data.get(prop, '')
+            writer.writerow(player_dict)
+
+
+player_prop_divs_containers = WebDriverWait(driver, 10).until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR, "div.styles__content___R51k")))
+player_prop_divs = []
+
+for player_prop_divs_container in player_prop_divs_containers:
+    current_player_prop_divs = player_prop_divs_container.find_elements(By.CSS_SELECTOR, "div.styles__overUnderCell__KgzNn")
+    for player_prop_div in current_player_prop_divs:
+        player_prop_divs.append(player_prop_div)
+
+data = []
+for player_prop_div in player_prop_divs:
+    player_name, props = find_player_and_props(driver, player_prop_div=player_prop_div)
+    player_data = {'Name': player_name}
+    for prop in props[1:]:
+        prop_name = prop.replace(" + ", "").split(" ")[1].strip()
+        if "PtsRebsAsts" in prop_name:
+            prop_name = f"PointsReboundsAssists"
+        if "3-Pointers" in prop_name:
+            prop_name = f"Threes"
+        prop_name = prop_name + "_Line"
+        value = prop.split(" ")[0].strip()
+        player_data[prop_name] = value
+    data.append(player_data)
+write_to_csv(data)
+driver.quit()
