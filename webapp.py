545cb1637b43addacbd714180e25191931bfce4c
David Weaver <davidweav322@gmail.com>
2024-02-10 19:07:19 -0500

    adds type

--- webapp.py
+++ webapp.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 from utils.get_data import return_data
 @app.route('/')
 def index():
-    return render_template('index.html')
+     return render_template('index.html')
 
 
 @app.route('/get_data', methods=['POST'])
